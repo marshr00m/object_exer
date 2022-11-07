e44f479318fc0c6ccf15dd4ef733cc36e36a24d6
marshr00m <marssub2300000@gmail.com>
2022-11-07 10:26:41 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileWriter out = new FileWriter("sample.txt"); //FileWriterオブジェクトを作成しファイルを開く
+
+            out.回答
+            ; //ファイル出力
+
+            out.回答
+            ; //ファイルを閉じる
+
+        } catch(IOException e) {
+
+            System.out.println("ファイル出力エラー");
+
+        }
+
+    }
+
+}
